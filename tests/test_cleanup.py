3d982e7a8cc7cba9b604e1d36b9fcf126dadc168
TimAdams84 <tim-adams@gmx.net>
2023-08-01 15:44:31 +0200

    Add tests for cleanup functions

--- tests/test_cleanup.py
+++ tests/test_cleanup.py
@@ -4,7 +4,8 @@ from unittest import TestCase
 import numpy as np
 import pandas as pd
 
-from clinical_etl_toolbox.cleanup import unify_number_format
+from clinical_etl_toolbox.cleanup import unify_number_format, clean_unknown_entries, \
+    remove_entries_with_inconsistent_datatypes
 
 
 class Test(TestCase):
@@ -16,3 +17,15 @@ class Test(TestCase):
     def test_unify_number_format(self):
         df_clean = unify_number_format(self.df)
         self.assertEqual(1500000, np.average(df_clean['cell_count'][9:16]))
+
+    def test_clean_unknown_entries(self):
+        df_clean = clean_unknown_entries(self.df)
+        self.assertEqual(pd.isna(df_clean['birth_date'])[13], True)
+        self.assertEqual(pd.isna(df_clean['birth_date'])[23], True)
+        self.assertEqual(pd.isna(df_clean['birth_date'])[24], True)
+        self.assertEqual(pd.isna(df_clean['freetext'])[22], True)
+        self.assertEqual(pd.isna(df_clean['freetext'])[27], True)
+
+    def test_remove_entries_with_inconsistent_datatypes(self):
+        df_clean = remove_entries_with_inconsistent_datatypes(self.df, threshold=0.5)
+        self.assertEqual(pd.isna(df_clean['sex'])[19], True)
