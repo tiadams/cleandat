db1333a2bf080cc86c075374e25d9b2ebe7ee663
TimAdams84 <tim-adams@gmx.net>
2023-08-02 12:40:47 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cleandat',
+    version='0.0.1',
+    packages=['tests', 'cleandat'],
+    url='https://github.com/tiadams/cleandat',
+    license='MIT License',
+    author='Tim Adams',
+    author_email='tim-adams@gmx.net',
+    description='Python functions to facilitate the pre-processing of data for ML tasks in a clinical context '
+)
